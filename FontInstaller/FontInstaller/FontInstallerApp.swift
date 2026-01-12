1c8ec696c5f0b0bad3ff391f7f9b8f71215c2d8b
허신일 <shintwl@icloud.com>
2026-01-12 11:46:13 +0900

    init project

---
+++ FontInstaller/FontInstaller/FontInstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontInstallerApp.swift
+//  FontInstaller
+//
+//  Created by ShinIl Heo on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FontInstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
